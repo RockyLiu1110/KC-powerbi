19e24085c6594a6ab9a79e7a1b66b424b78d832b
Rocky Liu <rockyliuuiuc@gmail.com>
2026-07-06 19:34:15 +0800

    Delete push_to_github.bat

--- push_to_github.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Administrator\Desktop\新建文件夹 (2)\KC\powerbi-prep"
-"C:\Program Files\Git\bin\bash.exe" -c "git add -A; git commit -m 'Add raw data, remove custom-visuals and guide doc'; git push; echo '--- Done! ---'; read -n 1"
-pause
